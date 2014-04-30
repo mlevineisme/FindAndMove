8c95754acc4af60d6525543867bef4bde0312e3a
Mike Levine <mlevineisme@gmail.com>
2014-04-30 16:29:55 -0700

    Refactored, adjusted begin/rescue blocks, removed unnecessary variables, clarified comments

--- FindAndMove.rb
+++ FindAndMove.rb
@@ -9,16 +9,16 @@ def print_err errorMsg
    puts $!.backtrace
 end
 
-CURRENT_FOLDER = FileUtils.pwd
-SOURCE_FOLDER = "c:/P4/test/pcatest/7.0/team/automation/TestScripts"
+#-- You may need to change the following variables as file extensions/locations change
+extensions_to_move = [".rb", ".xml"]
+source_folder = "c:/P4/test/pcatest/7.0/team/automation/TestScripts"
 DESTINATION_FOLDER = "C:/TestRuns/MovedScripts"
-puts "The Scriptlet Source Folder Is: #{SOURCE_FOLDER}"
-puts "Files specified in the .conf file will be copied to the destination folder: #{DESTINATION_FOLDER}\n\n"
-SCRIPTS_CONFIG_NAME = "#{File.basename(__FILE__, ".rb")}.conf"
+
+puts "Files matching the .conf found in #{source_folder} will be copied to #{DESTINATION_FOLDER}\n\n"
 
 #-- Open the directory configuration file
 begin
-   SCRIPTS_CONF_FILE = File.open("#{CURRENT_FOLDER}/#{SCRIPTS_CONFIG_NAME}")
+   SCRIPTS_CONF_FILE = File.open("#{FileUtils.pwd}/#{File.basename(__FILE__, ".rb")}.conf")
 rescue
    print_err "A problem arose when trying to open the scripts configuration file.
               Please verify it exists and is named to match the script file"
@@ -29,21 +29,20 @@ script_names = Array.new
 SCRIPTS_CONF_FILE.each_line {|line| script_names << line.chomp}
 script_files = []
 script_names.each {|line|
-   script_files << "#{line}.rb"
-   script_files << "#{line}.xml"
+   extensions_to_move.each {|ext|
+      script_files << "#{line}#{ext}"
+   }
 }
 
 #-- Copy the source files into the destination directory
-script_files.each do |file|
-   full_path = Dir.glob("#{SOURCE_FOLDER}/**/#{file}").first
-
-   begin
-      #-- Copy the files to their new directory
-         FileUtils.cp full_path, DESTINATION_FOLDER, :preserve => true
-         FileUtils.chmod("u=wrx,go=rx", "#{DESTINATION_FOLDER}\\#{file}") if (File.exist?("#{DESTINATION_FOLDER}\\#{file}")) and !(File.writable?("#{DESTINATION_FOLDER}\\#{file}"))
-         puts "#{full_path} was successfully copied to #{DESTINATION_FOLDER}"
-   rescue
-      print_err "A problem arose when trying to copy the files"
+begin
+   script_files.each do |file|
+      full_path = Dir.glob("#{source_folder}/**/#{file}").first
+      FileUtils.cp full_path, DESTINATION_FOLDER, :preserve => true
+      FileUtils.chmod("u=wrx,go=rx", "#{DESTINATION_FOLDER}\\#{file}") if (File.exist?("#{DESTINATION_FOLDER}\\#{file}")) and !(File.writable?("#{DESTINATION_FOLDER}\\#{file}"))
+      puts "#{full_path} was successfully copied to #{DESTINATION_FOLDER}"
    end
+rescue
+   print_err "A problem arose when trying to copy the files"
 end
 SCRIPTS_CONF_FILE.close
