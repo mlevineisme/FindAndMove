59714102d72634c6b22117c63ad074bdf7d54580
Mike Levine <mlevineisme@gmail.com>
2014-05-01 10:02:04 -0700

    Added file copy verification, granular rescues

--- FindAndMove.rb
+++ FindAndMove.rb
@@ -12,8 +12,7 @@ end
 #-- You may need to change the following variables as file extensions/locations change
 extensions_to_move = [".rb", ".xml"]
 source_folder = "c:/P4/test/pcatest/7.0/team/automation/TestScripts"
-DESTINATION_FOLDER = "C:/TestRuns/MovedScripts"
-
+DESTINATION_FOLDER = "C:\\TestRuns\\MovedScripts"
 puts "Files matching the .conf found in #{source_folder} will be copied to #{DESTINATION_FOLDER}\n\n"
 
 #-- Open the directory configuration file
@@ -36,13 +35,35 @@ script_names.each {|line|
 
 #-- Copy the source files into the destination directory
 begin
+   FileUtils::mkdir(DESTINATION_FOLDER) unless (Dir.exists?(DESTINATION_FOLDER))
+   filesEqual = []
    script_files.each do |file|
       full_path = Dir.glob("#{source_folder}/**/#{file}").first
-      FileUtils.cp full_path, DESTINATION_FOLDER, :preserve => true
-      FileUtils.chmod("u=wrx,go=rx", "#{DESTINATION_FOLDER}\\#{file}") if (File.exist?("#{DESTINATION_FOLDER}\\#{file}")) and !(File.writable?("#{DESTINATION_FOLDER}\\#{file}"))
-      puts "#{full_path} was successfully copied to #{DESTINATION_FOLDER}"
+
+      copy_successful = false
+      begin
+         copy_successful = true if (FileUtils.cp full_path, DESTINATION_FOLDER, :preserve => true)
+      rescue TypeError
+         print_err "Error reading the source file. Do the source files exist in the local workspace for team?"
+      rescue
+         print_err "A problem arose when trying to copy the files"
+      end
+
+      if copy_successful.eql?(true)
+         begin
+            FileUtils.chmod("u=wrx,go=rx", "#{DESTINATION_FOLDER}\\#{file}") if (File.exist?("#{DESTINATION_FOLDER}\\#{file}")) and !(File.writable?("#{DESTINATION_FOLDER}\\#{file}"))
+             if (File.mtime("#{DESTINATION_FOLDER}/#{file}".gsub("/", "\\")).eql?(File.mtime(full_path.gsub("/", "\\"))))
+               puts "#{full_path} was successfully copied to #{DESTINATION_FOLDER}"
+            else
+               puts "#{full_path} could not be copied to #{DESTINATION_FOLDER}!!!"
+            end
+         rescue
+            print_err "A problem arose when trying to change destination file permissions and verify the copy"
+         end
+      end
    end
 rescue
    print_err "A problem arose when trying to copy the files"
+ensure
+   SCRIPTS_CONF_FILE.close
 end
-SCRIPTS_CONF_FILE.close
